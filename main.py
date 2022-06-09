5b714ee10294aeeec840b4311b367ce4d51e3559
Ayon Paul <ayon.paul@stonybrook.edu>
2022-06-09 14:05:55 -0400

    Trying more fixes for deployment

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 app = FastAPI()
 connection_url = "mongodb+srv://apaul45:password123apaul@cluster0.qr58u.mongodb.net/?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+db = AsyncIOMotorClient()
+db.get_io_loop = asyncio.get_event_loop
 db = AsyncIOMotorClient(connection_url).torqatadb
-db.get_io_loop = asyncio.get_event_loop()
 imdb_collection = db.get_collection("imdb_shows")
 user_collection = db.get_collection("users")
 
